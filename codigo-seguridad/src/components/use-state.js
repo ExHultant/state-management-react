1981c0c1e02efc86f7ab8eb2508442f6fb2fe7ef
Ricardo Chavez <chavezmataricardo@gmail.com>
2024-08-05 22:59:52 -0400

    chore: Refactor code for security code validation and state management, cap 11

--- codigo-seguridad/src/components/use-state.js
+++ codigo-seguridad/src/components/use-state.js
@@ -11,6 +11,56 @@ function UseState() {
     confirmed: false,
   });
 
+  const onConfirm = () => {
+    setState({
+      ...state,
+      error: false,
+      loading: false,
+      confirmed: true,
+    });
+  };
+
+  const onError = () => {
+    setState({
+      ...state,
+      error: true,
+      loading: false,
+    });
+  };
+
+  const onWrite = (newValue) => {
+    setState({
+      ...state,
+      value: newValue,
+    });
+  };
+
+  const onCheck = () => {
+    setState({
+      ...state,
+      loading: true,
+      error: false,
+    });
+  };
+
+  const onDelete = () => {
+    setState({
+      ...state,
+      deleted: true,
+    });
+  };
+
+  const onReset = () => {
+    setState({
+      ...state,
+      value: "",
+      error: false,
+      loading: false,
+      deleted: false,
+      confirmed: false,
+    });
+  };
+
   React.useEffect(() => {
     if (!!state.loading) {
       console.log("Empezando...");
@@ -18,24 +68,15 @@ function UseState() {
         console.log("Validando...");
 
         if (state.value === SECURITY_CODE) {
-          setState({
-            ...state,
-            error: false,
-            loading: false,
-            confirmed: true,
-          });
+          onConfirm();
         } else {
-          setState({
-            ...state,
-            error: true,
-            loading: false,
-          });
+          onError();
         }
         console.log("Finalizada la validacion");
         console.log("Terminado");
       }, 3000);
     }
-  }, [state, state.loading, state.value]);
+  }, [state.loading]);
 
   if (!state.deleted && !state.confirmed) {
     return (
@@ -45,25 +86,18 @@ function UseState() {
 
         {state.error && !state.loading && <p>Error: Codigo incorrecto</p>}
         {state.loading && <p>Cargando...</p>}
+
         <input
           type="text"
           placeholder="Codigo de Seguridad"
           value={state.value}
           onChange={(event) => {
-            setState({
-              ...state,
-              value: event.target.value,
-              error: false,
-            });
+            onWrite(event.target.value);
           }}
         />
         <button
           onClick={() => {
-            setState({
-              ...state,
-              loading: true,
-              error: false,
-            });
+            onCheck();
           }}
         >
           Comprobar
@@ -75,27 +109,8 @@ function UseState() {
       <>
         <h1>¿Deseas Eliminar?</h1>
         <p>¿Estas Seguro que deseas eliminar el codigo de seguridad?</p>
-        <button
-          onClick={() =>
-            setState({
-              ...state,
-              deleted: true,
-            })
-          }
-        >
-          Si, Eliminar
-        </button>
-        <button
-          onClick={() =>
-            setState({
-              ...state,
-              confirmed: false,
-              value: "",
-            })
-          }
-        >
-          No, Cancelar
-        </button>
+        <button onClick={() => onDelete()}>Si, Eliminar</button>
+        <button onClick={() => onReset()}>No, Cancelar</button>
       </>
     );
   } else {
@@ -103,18 +118,7 @@ function UseState() {
       <>
         <h1>Eliminado</h1>
         <p>¡El componente ha sido eliminado con exito!</p>
-        <button
-          onClick={() =>
-            setState({
-              ...state,
-              confirmed: false,
-              deleted: false,
-              value: "",
-            })
-          }
-        >
-          Volver atras
-        </button>
+        <button onClick={() => onReset()}>Volver atras</button>
       </>
     );
   }
